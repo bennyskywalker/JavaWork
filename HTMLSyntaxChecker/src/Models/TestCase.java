9535d077c7709ebccac2991822c8d3759e27aded
Ben Tsui <bennyskywalker@rogers.com>
2013-10-30 19:02:27 -0400

    changes

---
+++ HTMLSyntaxChecker/src/Models/TestCase.java
@@ -0,0 +1,50 @@
+package Models;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: btsui
+ * Date: 30/10/13
+ * Time: 6:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestCase
+{
+    private List<String> testLines;
+    private int testCase;
+
+    /**
+     * Constructor
+     */
+    public TestCase(int testCase)
+    {
+        testLines = new ArrayList<String>();
+        this.testCase = testCase;
+    }
+
+    /**
+     * Adds a test line
+     * @param testLine
+     */
+    public void addTestLine(String testLine)
+    {
+        testLines.add(testLine);
+    }
+
+    public void debugStdOut()
+    {
+        Iterator iter = testLines.iterator();
+        int lineNum = 1;
+
+        while(iter.hasNext())
+        {
+            System.out.println("Line "+lineNum+": "+iter.next());
+            lineNum++;
+        }
+    }
+
+}
---
+++ HTMLSyntaxChecker/tests/Models/TestCaseTest.java
@@ -0,0 +1,28 @@
+package Models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: btsui
+ * Date: 30/10/13
+ * Time: 7:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestCaseTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAddTestLine() throws Exception {
+
+    }
+
+    @Test
+    public void testDebugStdOut() throws Exception {
+
+    }
+}
